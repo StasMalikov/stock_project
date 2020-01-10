d8d6215ff155ebe48301cbd5d87ce51fb5cd03ff
stas_malikov <stas.malikov75@gmail.com>
2020-01-10 17:51:25 +0300

    фиксы

--- app/__init__.py
+++ app/__init__.py
@@ -2,13 +2,14 @@ from flask import Flask, url_for
 from config import Config
 from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
-
+from app.utils import User
 
 UPLOAD_FOLDER = "app/static/img/".replace('/', '\\')
 app = Flask(__name__)
 app.config.from_object(Config)
 app.config['DEBUG'] = True
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['USER'] = User(-1 ,"", "", "no_auth", "")
 bootstrap = Bootstrap(app)
 login = LoginManager(app)
 
--- app/routes.py
+++ app/routes.py
@@ -6,8 +6,6 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from werkzeug.utils import secure_filename
 from app.utils import Utils, User
 
-user = User(-1 ,"", "", "no_auth", "")
-
 @app.route('/')
 @app.route('/index')
 def index():
@@ -26,9 +24,9 @@ def sign_in():
         if p_hash is not None:
             if check_password_hash(DbUtils.get_user_password(login), password):
                 tmp = DbUtils.select_user_full(login)
-                user = User(tmp[0][0], tmp[0][1], tmp[0][2], tmp[0][3], tmp[0][4])
+                app.config['USER'] = User(tmp[0][0], tmp[0][1], tmp[0][2], tmp[0][3], tmp[0][4])
                 users = DbUtils.select_users()
-                return render_template('manage_users.html', users = users, len=len(users))
+                return render_template('manage_users.html', users = users, len=len(users), login = app.config['USER'].login)
             else:
                 return render_template('index.html', messege = "Неправильный логин или пароль")
         else:
@@ -41,7 +39,7 @@ def sign_in():
 @app.route('/buy_prod_units', methods = ['GET','POST'])
 def buy_prod_units():
     products = Utils.get_prod_units()
-    return render_template('buy_prod_units.html', products = products, len_p = len(products), login="")
+    return render_template('buy_prod_units.html', products = products, len_p = len(products), login = app.config['USER'].login)
 
 @app.route('/manage_prod_units', methods = ['GET','POST'])
 def manage_prod_units():
@@ -50,7 +48,7 @@ def manage_prod_units():
         if action == "Удалить":
             DbUtils.delete_product_units(request.form['id'])
     products = Utils.get_prod_units()
-    return render_template('manage_prod_units.html', products = products, len_p = len(products))
+    return render_template('manage_prod_units.html', products = products, len_p = len(products), login = app.config['USER'].login)
 
 @app.route('/add_prod_unit', methods = ['GET','POST'])
 def add_prod_unit():
@@ -58,15 +56,15 @@ def add_prod_unit():
         action = request.form['s_btn']
         if action == "Поиск":
             names = DbUtils.select_products_names_find(request.form['find'])
-            return render_template('add_prod_unit.html', names = names, len_n = len(names))
+            return render_template('add_prod_unit.html', names = names, len_n = len(names), login = app.config['USER'].login)
         elif action == "Добавить продукт":
             prod_id = DbUtils.select_products_id(request.form['prod_name'].split()[0])[0][0]
             DbUtils.insert_product_units(prod_id, request.form['prod_count'], request.form['prod_date'], "for sale", request.form['price'])
             products = Utils.get_prod_units()
-            return render_template('manage_prod_units.html', products = products, len_p = len(products))
+            return render_template('manage_prod_units.html', products = products, len_p = len(products), login = app.config['USER'].login)
 
     names = DbUtils.select_products_names()
-    return render_template('add_prod_unit.html', names = names, len_n = len(names))
+    return render_template('add_prod_unit.html', names = names, len_n = len(names), login = app.config['USER'].login)
 
 @app.route('/manage_products', methods = ['GET','POST'])
 def manage_products():
@@ -77,13 +75,13 @@ def manage_products():
             factories = Utils.clear_select(prod.factory, DbUtils.select_factories_names()) 
             types = Utils.clear_select(prod.product_type, DbUtils.select_product_types_names()) 
             ingredients = Utils.clear_select(prod.ingridients, DbUtils.select_ingredients_name()) 
-            return render_template('edit_product.html', product = prod, factories = factories, types = types, ingredients = ingredients)        
+            return render_template('edit_product.html', product = prod, factories = factories, types = types, ingredients = ingredients, login = app.config['USER'].login)        
 
         elif action == "Удалить":
             DbUtils.delete_product(request.form['id'])
 
     products = Utils.get_products()
-    return render_template('manage_products.html', products = products, len_p = len(products))
+    return render_template('manage_products.html', products = products, len_p = len(products), login = app.config['USER'].login)
 
 @app.route('/edit_product', methods = ['GET','POST'])
 def edit_product():
@@ -118,12 +116,12 @@ def edit_product():
         DbUtils.insert_product_ingredients(request.form['id'], request.form.getlist('ingredient[]'))
 
         products = Utils.get_products()
-        return render_template('manage_products.html', products = products, len_p = len(products))
+        return render_template('manage_products.html', products = products, len_p = len(products), login = app.config['USER'].login)
         
     factories = DbUtils.select_factories_names()
     types = DbUtils.select_product_types_names()
     ingredients = DbUtils.select_ingredients()
-    return render_template('add_product.html', factories = factories, len_f = len(factories), types = types, len_t = len(types), ingredients = ingredients, len_i = len(ingredients))
+    return render_template('add_product.html', factories = factories, len_f = len(factories), types = types, len_t = len(types), ingredients = ingredients, len_i = len(ingredients), login = app.config['USER'].login)
 
 
 @app.route('/add_product', methods = ['GET','POST'])
@@ -162,12 +160,12 @@ def add_product():
 
         DbUtils.insert_product_ingredients(prod_id, request.form.getlist('ingredient[]'))
         products = Utils.get_products()
-        return render_template('manage_products.html', products = products, len_p = len(products))
+        return render_template('manage_products.html', products = products, len_p = len(products), login = app.config['USER'].login)
         
     factories = DbUtils.select_factories_names()
     types = DbUtils.select_product_types_names()
     ingredients = DbUtils.select_ingredients()
-    return render_template('add_product.html', factories = factories, len_f = len(factories), types = types, len_t = len(types), ingredients = ingredients, len_i = len(ingredients))
+    return render_template('add_product.html', factories = factories, len_f = len(factories), types = types, len_t = len(types), ingredients = ingredients, len_i = len(ingredients), login = app.config['USER'].login)
 
 
 @app.route('/manage_ingredients', methods = ['GET','POST'])
@@ -182,7 +180,7 @@ def manage_ingredients():
             DbUtils.delete_ingredients(request.form['id'])
 
     ingredients = DbUtils.select_ingredients()
-    return render_template('manage_ingredients.html', ingredients = ingredients, len = len(ingredients))
+    return render_template('manage_ingredients.html', ingredients = ingredients, len = len(ingredients), login = app.config['USER'].login)
 
 @app.route('/manage_product_type', methods = ['GET','POST'])
 def manage_product_type():
@@ -196,7 +194,7 @@ def manage_product_type():
             DbUtils.delete_product_types(request.form['id'])
 
     pr_types = DbUtils.select_product_types()
-    return render_template('manage_product_type.html', product_types = pr_types, len = len(pr_types))
+    return render_template('manage_product_type.html', product_types = pr_types, len = len(pr_types), login = app.config['USER'].login)
 
 # -------------------------FACTORIES------------------METHODS----------------
 
@@ -209,37 +207,37 @@ def edit_factory():
             formated_man = []
             for i in manufacturers:
                 formated_man.append(i[0]) 
-            return render_template('edit_factory.html', manufacturers = formated_man, id =request.form['id'], location = request.form['location'], fact_name = request.form['fact_name'], manuf_name =  request.form['manuf_name'])
+            return render_template('edit_factory.html', manufacturers = formated_man, id =request.form['id'], location = request.form['location'], fact_name = request.form['fact_name'], manuf_name =  request.form['manuf_name'], login = app.config['USER'].login)
         elif action == "Удалить":
             DbUtils.delete_factory(request.form['id'])
             factories = DbUtils.select_factories()
-            return render_template('manage_factories.html', factories = factories, len = len(factories))
-    return render_template('edit_factory.html')
+            return render_template('manage_factories.html', factories = factories, len = len(factories), login = app.config['USER'].login)
+    return render_template('edit_factory.html', login = app.config['USER'].login)
 
 @app.route('/add_factory', methods = ['GET','POST'])
 def add_factory():
     if request.method == 'POST':
         DbUtils.insert_factory(request.form['location'], request.form['fact_name'], request.form['manuf_name'])
         factories = DbUtils.select_factories()
-        return render_template('manage_factories.html', factories = factories, len = len(factories))
+        return render_template('manage_factories.html', factories = factories, len = len(factories), login = app.config['USER'].login)
     else:     
         manufacturers = DbUtils.select_manufacturers_name()
         formated_man = []
         for i in manufacturers:
             formated_man.append(i[0]) 
-        return render_template('add_factory.html', manufacturers = formated_man )
+        return render_template('add_factory.html', manufacturers = formated_man, login = app.config['USER'].login )
 
 @app.route('/update_factory', methods = ['GET','POST'])
 def update_factory():
     if request.method == 'POST':
         DbUtils.update_factory(request.form['id'], request.form['location'], request.form['fact_name'], request.form['manuf_name'])
     factories = DbUtils.select_factories()
-    return render_template('manage_factories.html', factories = factories, len = len(factories))
+    return render_template('manage_factories.html', factories = factories, len = len(factories), login = app.config['USER'].login)
 
 @app.route('/manage_factories', methods = ['GET','POST'])
 def manage_factories():
     factories = DbUtils.select_factories()
-    return render_template('manage_factories.html', factories = factories, len = len(factories))
+    return render_template('manage_factories.html', factories = factories, len = len(factories), login = app.config['USER'].login)
 
 @app.route('/manage_manufacturers', methods = ['GET','POST'])
 def manage_manufacturers():
@@ -253,22 +251,22 @@ def manage_manufacturers():
             DbUtils.delete_manufacturer(request.form['id'])
 
     manufacturers = DbUtils.select_manufacturers()
-    return render_template('manage_manufacturers.html', manufacturers = manufacturers, len=len(manufacturers))
+    return render_template('manage_manufacturers.html', manufacturers = manufacturers, len=len(manufacturers), login = app.config['USER'].login)
 
 
 # ----------------USERS---METHODS------------------------------------------------------------------
 @app.route('/manage_users')
 def manage_users():
     users = DbUtils.select_users()
-    return render_template('manage_users.html', users = users, len=len(users))
+    return render_template('manage_users.html', users = users, len=len(users), login = app.config['USER'].login)
 
 @app.route('/add_user', methods = ['GET','POST'])
 def add_user():
     if request.method == 'POST':
         DbUtils.insert_user(request.form['login'],request.form['password'], request.form['email'], request.form['user_type'], request.form['full_name'])
         users = DbUtils.select_users()
-        return render_template('manage_users.html', users = users, len=len(users))
-    return render_template('add_user.html')
+        return render_template('manage_users.html', users = users, len=len(users), login = app.config['USER'].login)
+    return render_template('add_user.html', login = app.config['USER'].login)
     
 @app.route('/update_user', methods = ['GET','POST'])
 def update_user():
@@ -282,9 +280,9 @@ def update_user():
         
         DbUtils.update_user(request.form['id'], request.form['login'], password_hash, request.form['email'], request.form['user_type'], request.form['full_name'])
         users = DbUtils.select_users()
-        return render_template('manage_users.html', users = users, len=len(users))
+        return render_template('manage_users.html', users = users, len=len(users), login = app.config['USER'].login)
     users = DbUtils.select_users()
-    return render_template('manage_users.html', users = users, len=len(users))
+    return render_template('manage_users.html', users = users, len=len(users), login = app.config['USER'].login)
 
 
 @app.route('/edit_user', methods = ['GET','POST'])
@@ -295,9 +293,9 @@ def edit_user():
             DbUtils.delete_user(request.form['id'])
 
             users = DbUtils.select_users()
-            return render_template("manage_users.html", users = users, len=len(users))
+            return render_template("manage_users.html", users = users, len=len(users), login = app.config['USER'].login)
         elif action_type == "Изменить":
-           return render_template('edit_user.html', id = request.form['id'],  login = request.form['login'], email= request.form['email'], full_name= request.form['full_name'], user_type= request.form['user_type'] )
+           return render_template('edit_user.html', id = request.form['id'],  login2 = request.form['login'], email= request.form['email'], full_name= request.form['full_name'], user_type= request.form['user_type'] , login = app.config['USER'].login)
 
-    return render_template('edit_user.html')
+    return render_template('edit_user.html', login = app.config['USER'].login)
     
